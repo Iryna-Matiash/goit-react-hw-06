fc388b30688c135a407dae56bf6caceba42fba0a
Iryna-Matiash <165329434+Iryna-Matiash@users.noreply.github.com>
2025-05-11 17:31:10 +0300

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,41 +1,49 @@
-//import { useState } from "react";//
-import css from "./App.module.css";
-import Navigation from "../Navigation/Navigation";
-import { Routes, Route } from "react-router-dom";
-//import HomePage from "../../pages/HomePage";
-//import MoviesPage from "../../pages/MoviesPage/MoviesPage";
-//import NotFoundPage from "../../pages/NotFoundPage";
-//import MovieDetailsPage from "../../pages/MovieDetailsPage/MovieDetailsPage";
-//import MovieCast from "../MovieCast/MovieCast";
-//import MovieReviews from "../MovieReviews/MovieReviews";
-import { lazy, Suspense } from "react";
-
-const MoviesPage = lazy(() => import("../../pages/MoviesPage/MoviesPage"));
-const HomePage = lazy(() => import("../../pages/HomePage"));
-const MovieDetailsPage = lazy(() =>
-  import("../../pages/MovieDetailsPage/MovieDetailsPage")
-);
-const NotFoundPage = lazy(() => import("../../pages/NotFoundPage"));
-
-const MovieCast = lazy(() => import("../MovieCast/MovieCast"));
-const MovieReviews = lazy(() => import("../MovieReviews/MovieReviews"));
+import ContactForm from "../ContactForm/ContactForm";
+import SearchBox from "../SearchBox/SearchBox";
+import ContactList from "../ContactList/ContactList";
+//import { useState, useEffect } from "react";
 
 export default function App() {
-  return (
-    <div className={css.container}>
-      <Navigation />
+  // const [data, setData] = useState(() => {
+  //   const savedData = JSON.parse(window.localStorage.getItem("savedData"));
 
-      <Suspense fallback={<p>Loading page...</p>}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/movies" element={<MoviesPage />} />
-          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-            <Route path="cast" element={<MovieCast />} />
-            <Route path="reviews" element={<MovieReviews />} />
-          </Route>
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Suspense>
+  //   return savedData ? savedData : İnitialData;
+  // });
+
+  //const [filter, setFilter] = useState("");
+
+  // useEffect(() => {
+  //   window.localStorage.setItem("savedData", JSON.stringify(data));
+  // }, [data]);
+
+  // const addData = (values) => {
+  //   const newData = {
+  //     name: values.name,
+  //     number: values.number,
+  //     id: nanoid(),
+  //   };
+
+  //   setData((prevData) => {
+  //     return [...prevData, newData];
+  //   });
+  // };
+
+  // const deleteData = (dataİd) => {
+  //   setData((prevData) => {
+  //     return prevData.filter((data) => data.id !== dataİd);
+  //   });
+  // };
+
+  // const filteredData = data.filter((el) =>
+  //   el.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
+  // );
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "modern-normalize";
 import App from "./components/App/App";
-import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </StrictMode>
 );
